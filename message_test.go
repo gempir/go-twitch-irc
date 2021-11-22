6a919e858b100c9fcc6220d051741479b3615ac5
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-11-22 12:39:00 +0100

    Move the test message to its own variable

--- message_test.go
+++ message_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestCanPraseBadActionMessageWithoutPanic(t *testing.T) {
-	//nolint
-	message := ParseMessage("@badges=;color=;display-name=pajlada;emotes=;mod=0;room-id=11148817;subscriber=0;tmi-sent-ts=1522855191000;turbo=0;user-id=11148817;user-type= :pajlada!pajlada@pajlada.tmi.twitch.tv PRIVMSG #pajlada :ACTION")
+	testMessage := "@badges=;color=;display-name=pajlada;emotes=;mod=0;room-id=11148817;subscriber=0;tmi-sent-ts=1522855191000;turbo=0;user-id=11148817;user-type= :pajlada!pajlada@pajlada.tmi.twitch.tv PRIVMSG #pajlada :ACTION"
+	message := ParseMessage(testMessage)
 	msg := message.(*PrivateMessage)
 
 	assertStringsEqual(t, "11148817", msg.User.ID)
